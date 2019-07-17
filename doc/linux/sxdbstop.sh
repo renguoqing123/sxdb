ebfb4643f339fca34380ae4608ad4c63bb15869e
renguoqing <1002674966@qq.com>
2019-07-17 16:10:42 +0800

    提交文件

--- doc/linux/sxdbstop.sh
+++ doc/linux/sxdbstop.sh
@@ -1,10 +1,10 @@
 now_date=$(date +%Y%m%d_%H%M%S)
-pid_8089=$(netstat -nlp | grep :8089 | awk '{print $7}' | awk -F"/" '{ print $1 }')
+pid_6609=$(netstat -nlp | grep :6609 | awk '{print $7}' | awk -F"/" '{ print $1 }')
 echo "当前时间：${now_date}"
-echo "进程pid_8089: ${pid_8089}"
-if [ ! $pid_8089 ]; then
-	echo "8089端口没有占用"
+echo "进程pid_6609: ${pid_6609}"
+if [ ! $pid_6609 ]; then
+	echo "6609端口没有占用"
 else
-	echo "关闭8089进程"
-	kill -9 $pid_8089
+	echo "关闭6609进程"
+	kill -9 $pid_6609
 fi
